6eb8af1a3a6559010227970042450372368765d6
mrmiruo02 <mrmiruo@gmail.com>
2025-03-03 14:22:47 +0800

    update city view

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -12,7 +12,7 @@
             <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
             <p class="text-sm mb-12">
                 {{
-                    new Date(weatherData.current.time).toLocaleDateString(
+                    new Date(weatherData.data.currentTime).toLocaleDateString(
                         "en-us",
                         {
                             weekday: "short",
@@ -22,7 +22,7 @@
                     )
                 }}
                 {{
-                    new Date(weatherData.current.time).toLocaleTimeString(
+                    new Date(weatherData.data.currentTime).toLocaleTimeString(
                         "en-us",
                         {
                             timeStyle: "short",
@@ -31,13 +31,22 @@
                 }}  
             </p>
             <p class="text-8xl mb-8">
-                {{ Math.round(weatherData.hourly.temperature_2m[0]) }}°C
+                {{ Math.round((weatherData.data.main.temp - 32)* 5/9) }}°C
             </p>
-            <!-- <div class="text-center">
-                <p class="capitalize">
-                    {{ weatherData. }}
-                </p>
-            </div> -->
+            <p>
+                Feels like
+                {{ Math.round((weatherData.data.main.feels_like - 32)* 5/9) }} &deg;
+            </p>
+            <p class="capitalize">
+                {{ weatherData.data.weather[0].description }}
+            </p>
+            <img
+              class="w-auto h-[50px] object-cover"
+              :src="
+                `http://openweathermap.org/img/wn/${weatherData.data.weather[0].icon}@2x.png`
+              "
+              alt=""
+            />
          </div>
     </div>
 </template>
@@ -49,17 +58,23 @@ const route = useRoute();
 const getWeatherData = async () => {
     try {
         const weatherDataResults = await axios.get(
-            `https://api.open-meteo.com/v1/forecast?latitude=${route.query.lat}&longitude=${route.query.lng}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`
+            `https://pro.openweathermap.org/data/2.5/weather?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=fc84e567667b28a5a8c9e9cff3b6acd9&units=imperial`
         );
 
-        return weatherDataResults.data;
+        // cal current date & time
+        const localOffset = new Date().getTimezoneOffset() * 60000;
+        const utc = weatherDataResults.data.dt * 1000 + localOffset;
+        weatherDataResults.data.currentTime =
+        utc + 1000 * weatherDataResults.data.timezone;
+        
+        return weatherDataResults;
 
     } catch (error){
         console.log(error);
     }
 };
 
-const weatherData = await getWeatherData(); 
+const weatherData = await getWeatherData();
 console.log(weatherData);
 
 </script>
